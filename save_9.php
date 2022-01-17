4c65d706be283e353a4efba2d0aedf12a994f8d3
v-Make <marlen_box@mail.ru>
2022-01-17 20:23:49 +0600

    task_9.php save_9.php

---
+++ save_9.php
@@ -0,0 +1,9 @@
+<?php
+$text=$_POST['text'];
+
+$pdo= new PDO("mysql:host=localhost;dbname=my_project;","root","");
+$sql="INSERT INTO my_table(text) VALUES(:text)";
+$statement=$pdo->prepare($sql);
+$statement->execute(['text' => $text]);
+
+header("Location:/task_9.php");
